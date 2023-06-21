536d0ee04779a59686de4400ad1f982e6db319ca
李东阳 <lidy4@allinpay.com>
2023-06-21 17:16:56 +0800

    vercel部署

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "../styles/layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Esun";
+const name = "李东阳";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
